dec95f6f4873739515aa8efb64eba2a7976ec04a
John Lloyd F. Sarraga <lloyds066@gmail.com>
2022-11-17 02:24:29 +0800

    Day 01 - Rock Band Name Generator
    Fun and simple python program that determines your possible Rock Band Name

---
+++ Day01 - RockBandNameGenerator.py
@@ -0,0 +1,8 @@
+# Create a greeting for your generator
+print("Welcome to the ultimate Rock Band Name Generator.")
+# Ask the user for the color of the shirt that they are wearing
+user_color = input("What's the color of your shirt?\n")
+# Ask the user for the last thing they ate earlier
+user_food = input("What is the last thing that you ate?\n")
+# Show the name of the band in new command line
+print("Your Rock Band Name could be:\n" + user_color + " " + user_food)
