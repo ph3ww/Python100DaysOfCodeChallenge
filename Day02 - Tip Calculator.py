70cd84eeb812b4182ff19d10bb754febeb1c5590
John Lloyd F. Sarraga <lloyds066@gmail.com>
2022-11-19 19:56:36 +0800

    Day02 - Tip Calculator
    Updated version:
    -fixes the final bill to constantly have 2 decimals

--- Day02 - Tip Calculator.py
+++ Day02 - Tip Calculator.py
@@ -16,7 +16,9 @@ final_tip = float(total_tip) / 100  # 12/100 = 0.12
 final_bill = float(total_bill) * float(final_tip)  # 150*0.12 = 18
 max_bill = float(total_bill) + float(final_bill)  # 150+18 = 168
 
-final_split = float(max_bill) / int(total_split)
+final_split = float(max_bill) / int(total_split)  # 168 / 5 = 33.6
 
 final_total = round(final_split, 2)
+# converts float to string with 2 decimal format
+final_total = "{:.2f}".format(final_split)
 print(f"Each person should pay ${final_total}")
